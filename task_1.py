e0f8be8343a1da13acb3384f64f4807701cc2b9b
shahzad055 <shankhan055@gmail.com>
2025-03-20 13:01:59 +0530

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,11 +1,12 @@
+try:
+    firstNumber = int(input("Enter the first number: "))
+    secondNumber = int(input("Enter the second number: "))
+    print(f"Addition : {firstNumber + secondNumber}")
 
-firstNumber = int(input("Enter the first number: "))
-secondNumber = int(input("Enter the second number: "))
+    print(f"Subtraction : {firstNumber - secondNumber}")
 
-print(f"Addition : {firstNumber + secondNumber}")
+    print(f"Multiplication : {firstNumber * secondNumber}")
 
-print(f"Subtraction : {firstNumber - secondNumber}")
-
-print(f"Multiplication : {firstNumber * secondNumber}")
-
-print(f"Division : {firstNumber / secondNumber}")
+    print(f"Division : {firstNumber / secondNumber}")
+except ValueError:
+    print("Enter valid Numbers!!")
