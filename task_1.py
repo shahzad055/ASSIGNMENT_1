bea67cb9b274f4dfb43815007751b25d6e94244b
shahzad055 <shankhan055@gmail.com>
2025-03-20 12:51:17 +0530

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,11 @@
+
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+
+print(f"Addition : {firstNumber + secondNumber}")
+
+print(f"Subtraction : {firstNumber - secondNumber}")
+
+print(f"Multiplication : {firstNumber * secondNumber}")
+
+print(f"Division : {firstNumber / secondNumber}")
